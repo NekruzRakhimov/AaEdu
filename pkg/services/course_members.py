b837dff179d673396dde31aa1afd1794f1cd7fc8
Nekr1010 <nramadonov@gmail.com>
2025-03-06 14:03:04 +0500

    changed git ignor

---
+++ pkg/services/course_members.py
@@ -0,0 +1,12 @@
+def course_members(user_id, course_id):
+    
+
+
+def add_member_to_course(user_id, course_id):
+    pass
+
+
+def delete_member_from_course(user_id, course_id):
+    pass
+
+def has_
