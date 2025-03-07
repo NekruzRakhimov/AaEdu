f8a6a35abc1b71acbe22f8396de6c45d7fa2211a
Nekr1010 <72392287+Nekr1010@users.noreply.github.com>
2025-03-07 18:46:05 +0500

    Update models.py

--- db/models.py
+++ db/models.py
@@ -50,7 +50,6 @@ class CourseUser(Base):
     __tablename__ = "course_users"
     course_id = Column(Integer, ForeignKey("courses.id"), primary_key=True)  # ID курса
     user_id = Column(Integer, ForeignKey("users.id"), primary_key=True)  # ID пользователя
-    role_in_course = Column(String)  # Роль пользователя в курсе ('student', 'teacher')
 
 
 # Таблица уроков (содержит информацию об уроках внутри курса)
@@ -128,8 +127,6 @@ class Event(Base):
     event_description = Column(Text)  # Описание события
     related_id = Column(Integer)  # ID связанного объекта (урока, курса, комментария и т. д.)
     created_at = Column(DateTime, default=datetime.datetime.now)  # Дата события
-    updated_at = Column(DateTime, default=datetime.datetime.now, onupdate=datetime.datetime.now)  # Дата обновления
-    deleted_at = Column(DateTime, nullable=True)  # Дата удаления
     only_admin_editable = Column(Boolean, default=True)  # Только администратор может редактировать
 
 
