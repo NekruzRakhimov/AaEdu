a49925df01cde88e21172b719d9e9ebdecfb3a02
Nekr1010 <nramadonov@gmail.com>
2025-03-07 18:26:34 +0500

    auth added

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ from db.models import migrate_tables
 from pkg.controllers.default import router as default_router
 from pkg.controllers.lesson import router as lesson_router
 from pkg.controllers.CourseController import router as courses_router
+from pkg.controllers.user import router as user_router
+from pkg.controllers.auth import router as auth_router
 from pkg.controllers.role import router as role_router
-
-
-
+from pkg.controllers.course_members import router as course_members_router
 
 if __name__ == "__main__":
     # Создание таблиц
@@ -23,6 +23,8 @@ if __name__ == "__main__":
     app.include_router(lesson_router)
     app.include_router(courses_router)
     app.include_router(role_router)
-
+    app.include_router(user_router)
+    app.include_router(auth_router)
+    app.include_router(course_members_router)
 
     uvicorn.run(app, port=settings.port, host=settings.host)
