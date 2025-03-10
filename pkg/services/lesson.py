661324006a4b5f6fc04e8ed9580511444743c64a
taronaj <jtaronad@gmail.com>
2025-03-10 12:49:48 +0500

    check role and check is course exists

--- pkg/services/lesson.py
+++ pkg/services/lesson.py
@@ -1,6 +1,21 @@
+from fastapi import HTTPException
 from db.models import Lesson
 from pkg.repositories import lesson as lesson_repository
+from pkg.repositories import user as user_repository
 from schemas.lesson import LessonSchema
+
+
+def is_admin_or_metnor(user_id: int):
+    user = user_repository.get_user_by_id(user_id)
+    if not user:
+        return False
+
+    return user.role_id == 1 or user.role_id == 2
+
+
+def is_user_has_access_to_lesson(user_id: int, course_id: int):
+    return lesson_repository.is_user_has_access_to_lesson(
+        user_id, course_id)
 
 
 def get_lessons(course_id: int):
@@ -14,8 +29,6 @@ def get_lesson_by_id(course_id: int, lesson_id: int):
 
 
 def create_lesson(course_id: int, lesson: LessonSchema):
-    # TODO: check if course exists
-
     l = Lesson()
     l.title = lesson.title
     l.description = lesson.description
@@ -26,10 +39,12 @@ def create_lesson(course_id: int, lesson: LessonSchema):
 
 
 def update_lesson(course_id: int, lesson_id: int, lesson: LessonSchema):
-    # TODO: check if course exists
-
     return lesson_repository.update_lesson(course_id, lesson_id, lesson)
 
 
 def delete_lesson(course_id: int, lesson_id: int):
     return lesson_repository.delete_lesson(course_id, lesson_id)
+
+
+def is_course_exists(course_id: int):
+    return lesson_repository.is_course_exists(course_id)
