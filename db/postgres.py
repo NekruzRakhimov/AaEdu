857e9de998fa6e0f48daca2da1b3d65372845cec
Sherzod <sherrzod05@gmail.com>
2025-03-11 21:59:05 +0300

    adding featrures

--- db/postgres.py
+++ db/postgres.py
@@ -3,5 +3,12 @@ from sqlalchemy.orm import sessionmaker, Session
 from configs.config import settings
 
 engine = create_engine(settings.database_url)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
+def get_db():
+    db: Session = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
